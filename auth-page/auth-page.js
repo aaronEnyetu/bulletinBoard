734d44000cbccc1f2e0066c15c3dda2ac6a945f8
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-18 10:39:34 -0700

    Added sign in user function

---
+++ auth-page/auth-page.js
@@ -0,0 +1,36 @@
+// import functions and grab DOM elements
+import { redirectIfLoggedIn, signupUser, signInUser } from '../fetch-utils.js';
+
+const signUpForm = document.getElementById('sign-up');
+
+const signInForm = document.getElementById('sign-in');
+
+
+redirectIfLoggedIn();
+// let state
+
+// set event listeners 
+  // get user input
+  // use user input to update state 
+  // update DOM to reflect the new state
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    console.log ({ email: data.get('email'), password: data.get('password') });
+    const user = await signupUser(data.get('email'), data.get('password'));
+
+    if (user) {
+        location.replace('/create-page');
+    }
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const user = await signInUser(data.get('email'), data.get('password'));
+
+    if (user) {
+        console.log(user);
+        location.replace('/create-page');
+    }
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,4 +1,3 @@
-//Enter Supabase information
 const SUPABASE_URL = 'https://wuqcwgxturbjhkdtdjwn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1cWN3Z3h0dXJiamhrZHRkanduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI3MjQ0MjMsImV4cCI6MTk2ODMwMDQyM30.qVkreSGtYAMaEk7THSwEY_1E0AB9q0iNMjuFXHaIa8Q';
 
@@ -13,6 +12,16 @@ export function getUser() {
 export async function signupUser(email, password) {
     const response = await client.auth.signUp({ email, password });
     if (response.user) {
+        return response.user;
+    } else {
+        console.error(response.error);
+    }
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.sigIn({ email, password });
+    if (response.user) {
+        console.log(response.user);
         return response.user;
     } else {
         console.error(response.error);
@@ -35,6 +44,20 @@ export async function redirectIfLoggedIn() {
     }
 }
 
+export async function fetchPosts() {
+    const response = await client.from('posts').select('*');
 
+    return response.data;
+}
+
+export async function createNewPost(post) {
+    const response = await client.from('posts').insert(post);
+    if (response.data) {
+        return response.data;
+
+    } else {
+        console.error(response.error);
+    }
+}
 
 
